4542163ba839837f42dbb901cd6314a963431485
Danish Craft <121832703+DanishCraftYT@users.noreply.github.com>
2025-09-20 14:51:50 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+function AltEngine() {
+    window.location.href = "https://www.youtube.com/watch?v=X_Wi--jB_5A";
+}
+
+function WordleDemake() {
+    window.location.href = "https://github.com/DanishCraftYT/WordleDemake";
+}
+
+function GithubAccount() {
+    window.location.href = "https://github.com/DanishCraftYT";
+}
+
+function ProgrammingYT() {
+    window.location.href = "https://www.youtube.com/@danishcraftstudios4023";
+}
